38efeef6a1257f73ecfb17a5c623164fe783b931
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-19 11:07:32 -0400

    Initial Commit

---
+++ Tip-Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip-Calculator
+//
+//  Created by Arsalan Vakili on 2015-06-19.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip-Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip-Calculator
+//
+//  Created by Arsalan Vakili on 2015-06-19.
+//  Copyright (c) 2015 Arsalan Vakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
