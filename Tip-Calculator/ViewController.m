a9457981d46e6bad3e1fb6e4d5d5255964c74964
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-19 15:34:54 -0400

    second commit

--- Tip-Calculator/ViewController.m
+++ Tip-Calculator/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
 
 @end
 
@@ -16,7 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +27,36 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)calculateTip:(id)sender {
+    
+    
+    //NSNumber *number= [[NSNumber alloc] initWithDouble:self.tipPercentageTextField.text];
+//    double billAmountEntered= [self.billAmountTextField.text doubleValue];
+//    double tipPercentageEntered= [self.tipPercentageTextField.text doubleValue]/100;
+//    double tipCalculated= billAmountEntered * tipPercentageEntered;
+//    NSString *mystring= [NSString stringWithFormat:@"%0.2f",tipCalculated];
+//    self.tipAmountLabel.text = mystring;
+    NSNumberFormatter *numberformat= [[NSNumberFormatter  alloc] init];
+    
+    numberformat.numberStyle= NSNumberFormatterDecimalStyle;
+    NSNumber *billTotal= [numberformat numberFromString:self.billAmountTextField.text];
+    NSNumber *percentagetip= [numberformat numberFromString:self.tipPercentageTextField.text];
+    
+    if (billTotal == nil || percentagetip== nil){
+        self.tipAmountLabel.text= @"please enter a valid number";
+    }
+    else{
+        double billAmountEntered=  [billTotal doubleValue ];
+        double tipPercentageEntered= [percentagetip doubleValue]  /100;
+        double tipCalculated= billAmountEntered * tipPercentageEntered;
+        NSString *mytipInString= [NSString stringWithFormat:@"%0.2f",tipCalculated];
+        self.tipAmountLabel.text= mytipInString;
+    }
+
+
+}
+- (IBAction)calculatorchange:(id)sender {
+}
+                                  
 @end
